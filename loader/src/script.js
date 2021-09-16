6d805dab91a44a7dd570fc24b91c36a0b03760c3
chiragmistry <chiragmistry@goldernmace.com>
2021-09-17 00:19:23 +0530

    -added new 3d assets

---
+++ loader/src/script.js
@@ -0,0 +1,124 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+// const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+let cube
+const loader = new GLTFLoader()
+loader.load('./cube.glb',( gltf ) => {
+    // called when the resource is loaded
+    scene.add(gltf.scene);
+    cube = gltf.scene.children[0]
+    gltf.scene.traverse( function ( child ) {
+        if ( child.isMesh ) {
+            console.log(child.material)
+            child.material.color = {r: 14,g: 27,b: 50}
+        }
+    })
+    tick()
+}
+)
+
+
+// Materials
+
+// const material = new THREE.MeshBasicMaterial()
+// material.color = new THREE.Color(0xff0000)
+
+// Mesh
+// const sphere = new THREE.Mesh(geometry,material)
+// scene.add(sphere)
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff, 0.1)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 5
+scene.add(camera)
+
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    cube.rotation.z = 3 * elapsedTime
+    cube.rotation.y = 2 * elapsedTime
+    cube.rotation.x = 3 * elapsedTime
+
+    // Update Orbital Controls
+    // controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+// tick()
